ed86b57d774106c013730db5ad4e8622cea76f6c
Dave <michaeldavegarciamello@gmail.com>
2022-11-20 21:55:56 +0800

    Update

--- app/src/main/java/com/dreambig/supplymanagementapp/Views/AuthFragments/SignInFragment/ForgotPasswordFragment.java
+++ app/src/main/java/com/dreambig/supplymanagementapp/Views/AuthFragments/SignInFragment/ForgotPasswordFragment.java
@@ -7,13 +7,26 @@ import androidx.annotation.Nullable;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.renderscript.ScriptGroup;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.dreambig.supplymanagementapp.MainActivity;
 import com.dreambig.supplymanagementapp.R;
 import com.dreambig.supplymanagementapp.databinding.FragmentForgotPasswordBinding;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthWebException;
+
+import org.jetbrains.annotations.NotNull;
 
 public class ForgotPasswordFragment extends Fragment {
 
@@ -39,5 +52,38 @@ public class ForgotPasswordFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        backListener();
+
+        submitListener();
+    }
+
+    private void backListener () {
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).popBackStack();
+            }
+        });
+    }
+
+    private void submitListener () {
+        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetPassword();
+            }
+        });
+    }
+
+    private  void resetPassword(){
+        String email = binding.etEmail.getText().toString().trim();
+        if (email.isEmpty()){
+            binding.etEmail.setError("Please enter your email");
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.etEmail.setError("Please enter a valid email");
+            return;
+        }
     }
 }
