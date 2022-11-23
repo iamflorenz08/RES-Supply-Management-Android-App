71a622c161a51cc17252528efe38177e84026ea2
iamflorenz08 <dejesusflorenz8@gmail.com>
2022-11-23 18:35:11 +0800

    "22.11.23.0634"

--- app/src/main/java/com/dreambig/supplymanagementapp/Networks/AuthService.java
+++ app/src/main/java/com/dreambig/supplymanagementapp/Networks/AuthService.java
@@ -2,6 +2,7 @@ package com.dreambig.supplymanagementapp.Networks;
 
 import com.dreambig.supplymanagementapp.Models.AuthResponseModel;
 import com.dreambig.supplymanagementapp.Models.RequestModel;
+import com.dreambig.supplymanagementapp.Models.ResetPasswordModel;
 import com.dreambig.supplymanagementapp.Models.ResponseModel;
 import com.dreambig.supplymanagementapp.Models.SignInModel;
 import com.dreambig.supplymanagementapp.Models.UserModel;
@@ -26,6 +27,9 @@ public interface AuthService {
 
     @POST("/auth/recover")
     Call<ResponseModel> sendEmail(@Body UserModel userModel);
+
+    @POST("/auth/reset")
+    Call<ResponseModel> resetPassword(@Header("Authorization") String token, @Body ResetPasswordModel newPassword);
 
     @POST("/user")
     Call<UserModel> getUserInfo(@Header("Authorization")String authHeader);
--- app/src/main/java/com/dreambig/supplymanagementapp/Views/AuthFragments/SignInFragment/CreateNewPasswordFragment.java
+++ app/src/main/java/com/dreambig/supplymanagementapp/Views/AuthFragments/SignInFragment/CreateNewPasswordFragment.java
@@ -4,22 +4,35 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.dreambig.supplymanagementapp.Locals.Token;
+import com.dreambig.supplymanagementapp.Models.ResponseModel;
 import com.dreambig.supplymanagementapp.R;
 import com.dreambig.supplymanagementapp.databinding.FragmentCreateNewPasswordBinding;
 
+import dagger.hilt.android.AndroidEntryPoint;
 
+@AndroidEntryPoint
 public class CreateNewPasswordFragment extends Fragment {
 
     private FragmentCreateNewPasswordBinding binding;
+    private Bundle bundle;
+    private String token;
+    private SignInViewModel mViewModel;
+    private AlertDialog alertDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,11 +45,74 @@ public class CreateNewPasswordFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SignInViewModel.class);
+        mViewModel.init();
 
-        Bundle bundle = getArguments();
-        Log.d("MY_DEV", bundle.getString("resetToken"));
+        //initialize details
+        init();
+
         //set status  bar
         setStatusBar();
+
+        //dialog builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder = new AlertDialog.Builder(getContext());
+        builder.setView(R.layout.progress_loading);
+        builder.setCancelable(false);
+        alertDialog = builder.create();
+
+        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isPasswordValid()){
+                    mViewModel.resetPassword(token, binding.etRepeatPassword.getText().toString());
+                    alertDialog.show();
+                }
+
+            }
+        });
+
+        mViewModel.getResponseResetPassword().observe(getViewLifecycleOwner(), new Observer<ResponseModel>() {
+            @Override
+            public void onChanged(ResponseModel responseModel) {
+                alertDialog.dismiss();
+                if (responseModel == null)
+                    return;
+
+                if(!responseModel.getError()){
+                    Toast.makeText(getContext(), responseModel.getMessage(), Toast.LENGTH_SHORT).show();
+                    Navigation.findNavController(binding.getRoot()).popBackStack();
+                    return;
+                }
+
+                Toast.makeText(getContext(), responseModel.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private Boolean isPasswordValid() {
+        binding.tilPassword.setError(null);
+        binding.tilRepeatPassword.setError(null);
+
+        if(binding.etPassword.getText().toString().length() < 6){
+            binding.tilPassword.setError("Password must be 6 characters above.");
+            return false;
+        }
+
+        if(!binding.etPassword.getText().toString().equals(binding.etRepeatPassword.getText().toString())){
+            binding.tilRepeatPassword.setError("Password does not match.");
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private void init(){
+        bundle = getArguments();
+        token = bundle.getString("resetToken");
     }
 
     private void setStatusBar() {
